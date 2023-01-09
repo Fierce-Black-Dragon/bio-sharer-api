ca1fc13a9bf13fba808634e96e33512698dd5917
Fierce-Black-Dragon <blackdragon4204@gmail.com>
2023-01-09 23:58:50 +0530

    login done

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-
+   resources :sessions, only: [:create]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root to: "static#home"
   # Defines the root path route ("/")
